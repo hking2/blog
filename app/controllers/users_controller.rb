5fc46b4db03a7b0e24bad752bf97310659239808
hking2 <hking6776@gmail.com>
2020-10-09 14:41:41 -0700

    pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Profile Updated Successfully"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
@@ -31,6 +31,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @articles = @user.articles
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   private
